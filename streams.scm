b8bf60fe2cb52d3c1fe007133bf8e022bf0e6937
Harish <harishtella@gmail.com>
2011-05-26 19:55:49 -0500

    complete EX 3.56, 3.59; add broken EX 3.60 solution

--- streams.scm
+++ streams.scm
@@ -4,21 +4,28 @@
 (define (scar s) (car s))
 (define (scdr s) (force (cdr s)))
 
-(define (sref s n) 
+; the nth elem of s
+(define (nth s n) 
   (if (= n 0) 
-      (stream-car s) 
-      (stream-ref (stream-cdr s) (- n 1)))) 
+      (scar s) 
+      (nth (scdr s) (- n 1)))) 
 
-(define (sget n s)
+; get n elems from S
+(define (take n s)
   (if (= n 0)
 	'()
-	(cons (scar s) (sget (- n 1) (scdr s)))))
- 
+	(cons (scar s) (take (- n 1) (scdr s)))))
+
+(define (list->stream lst)
+  (if (null? lst)
+	'()
+	(cons-stream (car lst) (list->stream (cdr lst)))))
+
 (define (stream-for-each proc s) 
   (if (stream-null? s) 
       'done 
-      (begin (proc (stream-car s)) 
-             (stream-for-each proc (stream-cdr s))))) 
+      (begin (proc (scar s)) 
+             (stream-for-each proc (scdr s))))) 
 
 (define (memo-proc proc) 
   (let ((already-run? false) (result false)) 
@@ -29,24 +36,86 @@
                  result)
            result)))) 
 
-(define (stream-map proc s1 s2) 
-  (if (or (stream-null? s1) 
-		  (stream-null? s2)) 
+(define (smap proc . sx) 
+  (if (stream-null? (scar sx))
       the-empty-stream 
-	  (cons-stream (proc (scar s1) (scar s2)) 
-				   (stream-map proc (scdr s1) (scdr s2)))))
+	  (cons-stream (apply proc (map scar sx))
+				   (apply smap (cons proc (map scdr sx))))))
 
 (define (add-streams s1 s2) 
-  (stream-map + s1 s2)) 
+  (smap + s1 s2)) 
 
 (define (mul-streams s1 s2)
-		(stream-map * s1 s2))
+		(smap * s1 s2))
 
 (define ones (cons-stream 1 ones)) 
 (define integers (cons-stream 1 (add-streams ones integers))) 
 
 ;EX 3.54
 (define factorials (cons-stream 1 (mul-streams factorials (scdr integers))))
+
+;EX 3.56
+(define (scale-stream stream factor) 
+  (smap (lambda (x) (* x factor)) stream)) 
+(define (merge s1 s2) 
+		(cond ((stream-null? s1) s2) 
+		  ((stream-null? s2) s1) 
+		  (else 
+			(let ((s1car (scar s1))
+				  (s2car (scar s2))) 
+			  (cond ((< s1car s2car) 
+					 (cons-stream s1car (merge (scdr s1) s2))) 
+				((> s1car s2car) 
+				 (cons-stream s2car (merge s1 (scdr s2)))) 
+				(else 
+				  (cons-stream s1car 
+							   (merge (scdr s1) 
+									  (scdr s2))))))))) 
+(define S (cons-stream 1 (merge (scale-stream S 2)
+								(merge (scale-stream S 3)
+									   (scale-stream S 5)))))
+
+
+;EX 3.59 a
+(define (integrate-series s)
+		(define (helper s ints)
+				(cons-stream (/ (scar s) 
+								(scar ints))
+							 (helper (scdr s) (scdr ints))))
+		(helper s integers))
+
+
+;EX 3.59b
+; wow really slick
+(define cosine-series 
+  (cons-stream 1 
+			   (smap (lambda (x) (* x -1))
+					 (integrate-series sine-series)))) 
+(define sine-series 
+  (cons-stream 0 
+			   (integrate-series cosine-series))) 
+
+; EX 3.60
+; this is not correct
+(define (mul-series s1 s2)
+		(cons-stream (* (scar s1) (scar s2))
+					 (add-streams
+					   (mul-series (scdr s1) s2)
+					   (mul-series s1 (scdr s2)))))
+(define (sqr-series s)
+		(mul-series s s))
+(define magic-number
+		(add-streams
+		  (sqr-series cosine-series)
+		  (sqr-series sine-series)))
+
+(define s (mul-series
+			(list->stream (list 5 2 0 0 0))
+			(list->stream (list 7 3 0 0 0))))
+
+
+
+
 
 
 
